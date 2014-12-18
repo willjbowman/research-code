90ab3e588bb2658a638f1a5a2daafd7a1ea5ae93
will <dingletron@WjB-PC.(none)>
2014-12-17 18:14:31 -0700

    added save output figure feature

--- python/calculate_concentration_eels.py
+++ python/calculate_concentration_eels.py
@@ -2,6 +2,7 @@ import add_modules_to_syspath # put ~/wills_modules in pyzo path variable
 import numpy as np
 import pylab as pl
 import matplotlib as mpl
+import wills_functions as wf
 from os import listdir # listdir(dir_path) returns array of file names in dir_path
 from os import mkdir
 from os import path
@@ -9,7 +10,7 @@ from os import path
 k_CaCe, k_OCe = 1.89, 5.81 # calculated from 10Ca 2d scans (x spectra)
 
 # specify path to directory of elemental map files
-maps_dir = "c:/Dropbox/SOFC Electrolyte Project/Microscopy/141007_2Ca_ARM200kV/gb_maps/"
+maps_dir = "c:/Dropbox/SOFC Electrolyte Project/Microscopy/140821_5CaDC_ARM200kV/gb_maps/"
 map_file_names = listdir( maps_dir ) # get map file names
 common_sub_str = 'EELS'
 sub_str_pattern = 'EELS_XX_'
@@ -33,6 +34,25 @@ def get_gb_id( file_name, sub_str ):
 if not path.isdir( maps_dir + 'calculated_concentrations' ):
     mkdir ( maps_dir + 'calculated_concentrations' )
 
+def calculate_concentration( i_CeM, i_CaL, i_OK ) :
+    kr_CaCe = i_CaL / i_CeM * k_CaCe
+    kr_OCe = i_OK / i_CeM * k_OCe
+        
+    c_Ca_i = kr_CaCe / ( 1 + kr_CaCe )
+    c_Ce_i = 1 - c_Ca_i
+    c_O_i = 2 - c_Ca_i
+    
+    return c_Ca_i, c_Ce_i, c_O_i
+
+        
+def save_concentration_map( x, c_Ce_i, c_Ca_i, c_O_i, maps_dir, id, gb_id, k_CaCe, k_OCe ) :         
+    output_data = np.vstack( ( x, c_Ce_i, c_Ca_i, c_O_i ) )
+    output_dir = maps_dir + 'calculated_concentrations/'
+    output_file = 'map_' + id + '_gb_' + gb_id + '.txt'
+    head = 'gb_id: ' + gb_id + '\nmap_id: ' + id + '\nk_CaCe: ' + str( k_CaCe ) + '\nk_OCe ' + str( k_OCe )
+        
+    np.savetxt( output_dir + output_file, output_data.T, delimiter='\t', header=head, fmt='%10.3e', newline='\n')
+
 for id in map_ids:
     if id.isdigit():
         file_group = [ ]
@@ -40,30 +60,47 @@ for id in map_ids:
         for file_name in map_file_names:
             if file_name.__contains__( '_' + id + '_' ):
                 file_group.append( maps_dir + file_name )
+                
+        # get gb id from file name
+        # gb_id = get_gb_id( file_group[0], '_gb' )
+        gb_id = wf.pluck_sub_string_counter( file_group[ 0 ], '_gb', 2 )
         
-        CeM = np.loadtxt( file_group[1] )
-        CaL = np.loadtxt( file_group[0] )
-        OK = np.loadtxt( file_group[2] )
+        screening_map = np.loadtxt( file_group[1], skiprows = 3 )
+        # check map dimensions for 1d or 2d spectrum images
+        map_cols = screening_map.shape[ 1 ]
         
-        gb_id = get_gb_id( file_group[0], '_gb' )
-    
-        x = CeM[ :, 0 ]
-        
-        i_CeM = CeM[ :, 1 ]
-        i_CaL = CaL[ :, 1 ]
-        i_OK = OK[ :, 1 ]
-        
-        kr_CaCe = i_CaL / i_CeM * k_CaCe
-        kr_OCe = i_OK / i_CeM * k_OCe
-        
-        c_Ca_i = kr_CaCe / ( 1 + kr_CaCe )
-        c_Ce_i = 1 - c_Ca_i
-        c_O_i = 2 - c_Ca_i
-        
-        output_data = np.vstack( ( x, c_Ce_i, c_Ca_i, c_O_i ) )
-        output_dir = maps_dir + 'calculated_concentrations/'
-        output_file = 'map_' + id + '_gb_0' + gb_id + '.txt'
-        head = 'gb_id: ' + gb_id + '\nmap_id: ' + id + '\nk_CaCe: ' + str( k_CaCe ) + '\nk_OCe ' + str( k_OCe )
-        
-        np.savetxt( output_dir + output_file, output_data.T, delimiter='\t', header=head, fmt='%10.3e', newline='\n')
-
+        if map_cols > 2 :
+            # 2d map
+            CeM = np.loadtxt( file_group[1], skiprows = 3 ) # load concentration maps data
+            CaL = np.loadtxt( file_group[0], skiprows = 3 )
+            OK = np.loadtxt( file_group[2], skiprows = 3 )
+            for i in range( 1, map_cols ) :
+                x = CeM[ :, 0 ] # distance exported from DM as 1st column
+                i_CeM = CeM[ :, i ]
+                i_CaL = CaL[ :, i ]
+                i_OK = OK[ :, i ]
+                
+                c_Ca_i, c_Ce_i, c_O_i = calculate_concentration( i_CeM, i_CaL, i_OK )
+                
+                id_counter = 0.1 # create subcounter string for file name
+                id_int = float( id ) + id_counter
+                id_form = '%.1f' % id_int
+                id = id_form
+#                 print( id_float, id_int, id )
+                
+                # save
+                save_concentration_map( x, c_Ce_i, c_Ca_i, c_O_i, maps_dir, id, gb_id, k_CaCe, k_OCe )
+        else :
+            # 1d map
+            x = CeM[ :, 0 ] # distance exported from DM as 1st column
+            CeM = np.loadtxt( file_group[1] ) # load concentration maps data
+            CaL = np.loadtxt( file_group[0] )
+            OK = np.loadtxt( file_group[2] )
+            i_CeM = CeM[ :, 1 ]
+            i_CaL = CaL[ :, 1 ]
+            i_OK = OK[ :, 1 ]
+            
+            c_Ca_i, c_Ce_i, c_O_i = calculate_concentration( i_CeM, i_CaL, i_OK )
+            
+            # save
+            save_concentration_map( x, c_Ce_i, c_Ca_i, c_O_i, maps_dir, id, gb_id, k_CaCe, k_OCe )
--- python/plot_gb_concentration_profiles.py
+++ python/plot_gb_concentration_profiles.py
@@ -11,49 +11,64 @@ from os import path
 # filename: 'map_xx_gb_y' where xx and yy are counters
 
 # specify path to directory of elemental map files
-maps_dir = "c:/Dropbox/SOFC Electrolyte Project/Microscopy/141007_2Ca_ARM200kV/gb_maps/calculated_concentrations/"
+maps_dir = "c:/Dropbox/SOFC Electrolyte Project/Microscopy/141118_10Ca_ARM200kV/gb_maps/calculated_concentrations/"
 map_files = listdir( maps_dir ) # get map file names
 
 pl.close( 'all' )
 
+def label_and_save_current():
+    ax = pl.gca()
+    title = 'GB_' + str( gb_num )
+    ax.set_title( title )
+    ax.set_xlabel( 'distance (nm)' )
+    ax.set_ylabel( 'Concentration' )
+    
+    if title != 'GB_None' :
+        output_dir = path.join( maps_dir, 'plots/' )
+        if not path.exists( output_dir ) :
+            mkdir( output_dir )
+        pl.savefig( output_dir + title + '.png', format = 'png', dpi = 500 )
+
 gb_num = None
 for file in map_files:
-    gb_num_i = wf.pluck_sub_string_counter( file, '_gb_', 2 )
-    d = np.loadtxt( maps_dir + file )
+    if file.endswith( '.txt' ) :
+        gb_num_i = wf.pluck_sub_string_counter( file, '_gb_', 2 )
+        d = np.loadtxt( maps_dir + file )
     
 #     wf.shift_curve( d, x, 0 )
-    x = d[ :, 0 ]
-    y = d[ :, 1 ]
-    min_y = np.min( y )
-    max_y = np.max( y )
-    mean_y = np.mean( y )
-    if ( max_y - mean_y ) > ( mean_y - min_y ) :
-        yy = y.tolist()
-        shift_index = yy.index( max_y )
-        shift_value = x[ shift_index ]
-    else:
-        yy = y.tolist()
-        shift_index = yy.index( min_y )
-        shift_value = x[ shift_index ]
+        x = d[ :, 0 ]
+        y = d[ :, 1 ]
+        min_y = np.min( y )
+        max_y = np.max( y )
+        mean_y = np.mean( y )
+        if ( max_y - mean_y ) > ( mean_y - min_y ) :
+            yy = y.tolist()
+            shift_index = yy.index( max_y )
+            shift_value = x[ shift_index ]
+        else:
+            yy = y.tolist()
+            shift_index = yy.index( min_y )
+            shift_value = x[ shift_index ]
     
-    col_num = map_files.index( file )
-    col = wf.color_list( col_num )
-    mark = wf.marker_list( col_num )
+        col_num = map_files.index( file )
+        col = wf.color_list( col_num )
+        mark = wf.marker_list( col_num )
     
-    if gb_num_i == gb_num:
-        wf.plot_multiple_1d( d, color = col, shift = ( shift_value, 0 ), style = mark )
+        if gb_num_i == gb_num:
+            wf.plot_multiple_1d( d, color = col, shift = ( shift_value, 0 ), style = mark )
             
-    else:
-        pl.figure()
-        wf.plot_multiple_1d( d, color = col, shift = ( shift_value, 0 ), style = mark )
-    
-    ax = pl.gca()
+        else:
+            # label previous figure if it exists
+            label_and_save_current()
+            # or create one if it doesn't
+            pl.figure() # create new figure
+            wf.plot_multiple_1d( d, color = col, shift = ( shift_value, 0 ), style = mark )
         
-    gb_num = gb_num_i
-    
+        gb_num = gb_num_i
 
-# from wills_eels_modules import plot_gb_composition
-# plot_gb_composition( output_dir )
+# the last iteration will result in a figure that needs to be saved
+label_and_save_current()
+    
 ##
 
 '''
