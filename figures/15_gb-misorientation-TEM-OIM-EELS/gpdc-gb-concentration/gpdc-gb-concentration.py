0551ba1a569ab46d695e45aa2858ca35bc43c0e4
willjbowman <willbowman35@gmail.com>
2015-09-03 13:15:05 +0200

    working on gpdb gb concentration figure

--- figures/15_gb-misorientation-TEM-OIM-EELS/gpdc-gb-concentration/gpdc-gb-concentration.py
+++ figures/15_gb-misorientation-TEM-OIM-EELS/gpdc-gb-concentration/gpdc-gb-concentration.py
@@ -17,87 +17,74 @@ import csv, imp, os
 
 ''' ########################### USER-DEFINED ########################### '''
 # path to data file (make gui to pick files?)
-data_dir = 'C:/Users/willb/Dropbox/WillB/Crozier_Lab/Writing/2015_gb misorientation OIM EELS/figures/gpdc-gb-eels/'
+data_dir = 'C:/Users/willb/Dropbox/WillB/Crozier_Lab/Writing/2015_gb misorientation OIM EELS/figures/gpdc-gb-concentration/'
 
-# d: 140203 SI-02; e: 140729 SI-11; f: 140203 SI-08
-eels_d = data_dir + 'eels-data/140203_3aGPDCfib_EELSSI2_highloss_gbAB.txt'
-eels_e = data_dir + 'eels-data/140729_gpdcFIB_EELSSI-12_gb5.txt'
-eels_f = data_dir + 'eels-data/140203_3aGPDCfib_EELSSI8_highloss_gbAE.txt'
-
-conc_g = data_dir + 'composition-profiles/140203_3aGPDCfib_EELSSI2_highloss_gbAB_1LO.txt'
-conc_h = data_dir + 'composition-profiles/140729_gpdcFIB_EELSSI-11_gb5_1FH.txt'
-conc_i = data_dir + 'composition-profiles/140203_3aGPDCfib_EELSSI8_highloss_gbAE_1OP.txt'
+# concentration data; 
+d_conc_ang = data_dir + 'gpdc-gb-conc-v-angle_150828.txt'
+d_length_conc = data_dir + 'gpdc-gb-length-v-conc_150903.txt'
 
 # output file info
 output_dir = data_dir
-output_file = 'gpdc-gb-eels_d-i'
+output_file = 'gpdc-gb-concentration'
 
 # font size, resolution (DPI), file type
 fsize, dots, file_type = 10, [300,1200], 'png'
 
-# eels plots
-eels_x_lab, eels_y_lab = 'Energy loss (eV)', 'Counts\n(Arb. units)'
-eels_entries = ( 'Grain', 'G.B.' )
-eels_x, eels_y = [ 860, 1250 ], [ -1e3, None ]
-c_eels, ls_on = 'maroon', (4,1)
-percent_shift = 200
-eels_x_maj_loc = 100
+# subplot 0 (sp0): concentration v angle plot
+sp0_x_lab, sp0_y_lab = 'Misorientation angle (Deg.)', 'Concentration (Mole frac.)'
+sp0_entries = ( 'CeGr.', 'CeG.B.', 'GdGr.', 'GdG.B.', 'PrGr.', 'PrG.B.' ) # legend
+sp0_c = [ 'maroon', wf.hex_gold(), 'black' ] # line colors
+sp0_m = [ 'o', 'D', 's' ] # markers
+sp0_x, sp0_y = [ 15, 65 ], [ 0, 1 ] # axis limits
+sp0_maj_loc = [ 10, None ] # [x,y] major tick locators
 
-# concentration profiles
-conc_x_lab, conc_y_lab = 'Distance (nm)', 'Concentration\n(Mole frac.)'
-conc_entries = ( 'Ce', 'Gd', 'Pr' )
-g_conc_x, h_conc_x, i_conc_x, ticks_x = [ 5, 15 ], [ 3, 13 ], [ 3, 13 ], 4
-conc_x_maj_loc, conc_y_maj_loc = 2, 1
-x_tick_labs = [ None, -4 , -2, 0, 2, 4 ]
-g_shift_x, h_shift_x, i_shift_x = 0.5, 1, -0.5
+# subplot1 (sp1): length fraction v concentration
+sp1_x_lab, sp1_y_lab = 'Concentration (Mole fract.)', 'Length frac.'
+sp1_entries = ( 'Pr', 'Gd', 'Ce' )
+sp1_c = [ 'black', wf.hex_gold(), 'maroon' ] # line colors
+sp1_x, sp1_y = [ 15, 65 ], [ 0, 1 ] # axis limits
+# sp1_maj_loc = [ 10, None ] # [x,y] major tick locators
+# x_tick_labs = [ None, -4 , -2, 0, 2, 4 ] # tick labels
 conc_y = [ 0, 1 ]
-ce_m, gd_m, pr_m = 'o', 'D', 's'
-ce_c, gd_c, pr_c = wf.hex_gold(), 'black', 'grey'
+
 
 ''' ########################### FUNCTIONS ########################### '''
 
 def mpl_customizations():
     wf.wills_mpl()
+    # wf.nanoletters_mpl()
     mpl.rc( 'font', family='sans-serif', serif='Helvetica', weight='normal',size=fsize )
     mpl.rc( 'lines', linewidth=1.0, mew=0.01, markersize=3 )
     mpl.rc( 'legend', borderpad = 0.1, labelspacing = 0.1 )
-    
-def stack( top_curve, bottom_curve ):
-    shifted_top = top_curve + np.max( bottom_curve ) - np.min( bottom_curve )
-    return shifted_top
 
-def eels_style( ax ):
-    pl.xlim( eels_x )
-    pl.ylim( eels_y )
-    pl.minorticks_on()
-    ax.xaxis.set_major_locator( mpl.ticker.MultipleLocator( eels_x_maj_loc ) )
-    ax.yaxis.set_ticklabels([]) # y tick labels off
+def sp0_style( ax ):
+    # pl.xlim( eels_x )
+    # pl.ylim( eels_y )
+    # pl.minorticks_on()
+    # ax.xaxis.set_major_locator( mpl.ticker.MultipleLocator( eels_x_maj_loc ) )
+    # ax.yaxis.set_ticklabels([]) # y tick labels off
 
-def conc_style( ax ):
-    pl.ylim( conc_y )
-    pl.minorticks_on()
-    ax.xaxis.set_major_locator( mpl.ticker.MultipleLocator( conc_x_maj_loc ) )
-    ax.yaxis.set_major_locator( mpl.ticker.MultipleLocator( conc_y_maj_loc ) )
-    ax.xaxis.set_ticklabels( x_tick_labs ) # x tick labels
-    
+def sp1_style( ax ):
+    # pl.ylim( conc_y )
+    # pl.minorticks_on()
+    # ax.xaxis.set_major_locator( mpl.ticker.MultipleLocator( conc_x_maj_loc ) )
+    # ax.yaxis.set_major_locator( mpl.ticker.MultipleLocator( conc_y_maj_loc ) )
+    # ax.xaxis.set_ticklabels( x_tick_labs ) # x tick labels
+
+
 ''' ########################### MAIN SCRIPT ########################### '''
 # read data, skip header rows
-d_ev, d_off, d_on = np.loadtxt( eels_d, skiprows = 2 ).T # REFACTOR THIS
-e_ev, e_off, e_on = np.loadtxt( eels_e, skiprows = 2 ).T
-f_ev, f_off, f_on = np.loadtxt( eels_f, skiprows = 2 ).T
-
-g_nm, g_pr, g_gd, g_ce = np.loadtxt( conc_g, skiprows = 2 ).T
-h_nm, h_pr, h_gd, h_ce = np.loadtxt( conc_h, skiprows = 2 ).T
-i_nm, i_pr, i_gd, i_ce = np.loadtxt( conc_i, skiprows = 2 ).T
+angle, ce_gr, ce_gb, ce_av, pr_gr, pr_gb, _pr_av, gd_gr, gd_gb, gd_av = np.loadtxt( d_conc_ang, skiprows = 1 ).T
+conc_bin, pr_len, gd_len, ce_len = np.loadtxt( d_length_conc, skiprows = 1 ).T
 
 pl.close( 'all' ) # close all open figures
-pl.figure( figsize = ( 7, 2.8 ) ) # create a figure of size ( width", height" )
+pl.figure( figsize = ( 3.33, 6.66 ) ) # create a figure of size ( width", height" )
 ax = pl.gca() # store current axis
 
 wf.mpl_customizations() # apply customizations to matplotlib
 # wf.slide_art_styles() # figure styling
 
-pl.subplot( 2, 3, 1 ) # ( sub_y, sub_x, sub_i )
+pl.subplot( 2, 1, 1 ) # ( sub_y, sub_x, sub_i )
 ax1 = pl.gca()
 pl.plot( d_ev, stack( d_off, d_on ), c=c_eels )
 pl.plot( d_ev, d_on, c=c_eels, dashes=ls_on )
@@ -105,50 +92,12 @@ eels_style( ax1 )
 pl.ylabel( eels_y_lab, labelpad=0.5 )
 pl.legend( eels_entries, loc='upper center', fontsize=fsize )
 
-pl.subplot( 2, 3, 2 ) # ( sub_y, sub_x, sub_i )
+pl.subplot( 2, 1, 2 ) # ( sub_y, sub_x, sub_i )
 ax2 = pl.gca()
 pl.plot( e_ev, stack( e_off, e_on ), c=c_eels )
 pl.plot( e_ev, e_on, c=c_eels, dashes=ls_on )
 eels_style( ax2 )
 pl.xlabel( eels_x_lab, labelpad=0.3 )
-
-pl.subplot( 2, 3, 3 ) # ( sub_y, sub_x, sub_i )
-ax3 = pl.gca()
-pl.plot( f_ev, stack( f_off, f_on ), c=c_eels )
-pl.plot( f_ev, f_on, c=c_eels, dashes=ls_on )
-eels_style( ax3 )
-
-
-pl.subplot( 2, 3, 4 ) # ( sub_y, sub_x, sub_i )
-ax4 = pl.gca()
-pl.plot( g_nm+g_shift_x, g_ce, marker=ce_m, c=ce_c )
-pl.plot( g_nm+g_shift_x, g_gd, marker=gd_m, c=gd_c )
-pl.plot( g_nm+g_shift_x, g_pr, marker=pr_m, c=pr_c )
-pl.xlim( g_conc_x )
-conc_style( ax4 )
-pl.ylabel( conc_y_lab, labelpad=0.5 )
-pl.legend( conc_entries, loc='center left', fontsize=fsize, numpoints = 3 )
-
-
-pl.subplot( 2, 3, 5 ) # ( sub_y, sub_x, sub_i )
-ax5 = pl.gca()
-pl.plot( h_nm+h_shift_x, h_ce, marker=ce_m, c=ce_c )
-pl.plot( h_nm+h_shift_x, h_gd, marker=gd_m, c=gd_c )
-pl.plot( h_nm+h_shift_x, h_pr, marker=pr_m, c=pr_c )
-pl.xlim( h_conc_x )
-conc_style( ax5 )
-ax5.yaxis.set_ticklabels([]) # y tick labels off
-pl.xlabel( conc_x_lab, labelpad=0.3 )
-
-
-pl.subplot( 2, 3, 6 ) # ( sub_y, sub_x, sub_i )
-ax6 = pl.gca()
-pl.plot( i_nm+i_shift_x, i_ce, marker=ce_m, c=ce_c )
-pl.plot( i_nm+i_shift_x, i_gd, marker=gd_m, c=gd_c )
-pl.plot( i_nm+i_shift_x, i_pr, marker=pr_m, c=pr_c )
-pl.xlim( i_conc_x )
-ax6.yaxis.set_ticklabels([]) # y tick labels off
-conc_style( ax6 )
 
 # applies to all subplots, h_pad defined vertical spacing
 pl.tight_layout( pad=0.3, h_pad=0.6 )
